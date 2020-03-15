05f4e6355998df2417b25a057d11408991ac608f
Wojtek Wojtunik <desqa16@gmail.com>
2020-03-15 20:35:36 +0100

    Updates

---
+++ precache-manifest.b3c836cc56864f3101a0df80268d53d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abf1e21754a7f724f391849d1a0ce71",
+    "url": "/Portmonetka/index.html"
+  },
+  {
+    "revision": "cdfaa9b0c452e013f309",
+    "url": "/Portmonetka/static/css/main.b6d5d62a.chunk.css"
+  },
+  {
+    "revision": "176fe4d2d1a118b77a6a",
+    "url": "/Portmonetka/static/js/2.18072f43.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/Portmonetka/static/js/2.18072f43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdfaa9b0c452e013f309",
+    "url": "/Portmonetka/static/js/main.882c8d9c.chunk.js"
+  },
+  {
+    "revision": "74438b73751e68f17bf8",
+    "url": "/Portmonetka/static/js/runtime-main.774edea4.js"
+  }
+]);
